ada3878e2d3198a1ab928f190ad25b3d904bcc7f
anirudhct <anirudh@connectia.in>
2025-12-11 12:44:46 +0530

    api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
       setLoading(true);
 
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/users`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/users`,
         form
       );
 
